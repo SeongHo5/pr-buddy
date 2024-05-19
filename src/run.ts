a14cf890f573631bf2bde186ad37827af6ba0c36
SeongHo5 <newfuring1123@gmail.com>
2024-05-19 19:24:02 +0900

    feat: 실행 함수 추가

---
+++ src/run.ts
@@ -0,0 +1,31 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import * as utils from './utils'
+import * as handler from './handler'
+import {Config, ConfigOption} from "./types";
+
+export async function run() {
+    try {
+        const token: string = core.getInput('repo-token', {required: true})
+        const configPath: string = core.getInput(
+            'configuration-path',
+            {required: true,}
+        );
+
+        const client = github.getOctokit(token);
+        const {repo, sha} = github.context;
+        const option: ConfigOption = {
+            owner: repo.owner,
+            repo: repo.repo,
+            path: configPath,
+            ref: sha
+        };
+        const config: Config = await utils.fetchConfigFileFrom(client, option);
+
+        await handler.startPRAutoAssign(client, github.context, config);
+    } catch (error) {
+        if (error instanceof Error) {
+            core.setFailed(error.message);
+        }
+    }
+}
